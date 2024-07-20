6bd7aa0751b596de6ad835b2ade0bf8c199b359b
Khang Hua <khanghua1505@gmail.com>
2024-07-20 17:31:51 +0700

    init project

--- bin/eks-playground.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { EksPlaygroundStack } from '../lib/eks-playground-stack';
-
-const app = new cdk.App();
-new EksPlaygroundStack(app, 'EksPlaygroundStack');
