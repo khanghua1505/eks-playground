5acd20a0d6aa57b76334de23f441ac12984d4092
Khang Hua <khanghua1505@gmail.com>
2024-07-29 09:40:14 +0700

    update EKS add-ons constructs

---
+++ lib/constructs/CoreDnsAddOn.ts
@@ -0,0 +1,39 @@
+import {Construct} from 'constructs';
+import {KubernetesVersion} from 'aws-cdk-lib/aws-eks';
+import {CoreAddon, CoreAddOnProps} from './CoreAddon';
+
+const versionMap: Map<KubernetesVersion, string> = new Map([
+  [KubernetesVersion.V1_30, 'v1.11.1-eksbuild.8'],
+  [KubernetesVersion.V1_29, 'v1.11.1-eksbuild.4'],
+  [KubernetesVersion.V1_28, 'v1.10.1-eksbuild.4'],
+  [KubernetesVersion.V1_27, 'v1.10.1-eksbuild.4'],
+  [KubernetesVersion.V1_26, 'v1.9.3-eksbuild.7'],
+]);
+
+/**
+ * Configuration options for the coredns add-on.
+ */
+export type CoreDnsAddOnProps = Omit<
+  CoreAddOnProps,
+  'versionMap' | 'serviceAccountName' | 'addOnName'
+>;
+
+const defaultProps = {
+  addOnName: 'coredns',
+  serviceAccountName: 'coredns',
+  configurationValues: {},
+  versionMap,
+};
+
+/**
+ * Implementation of CoreDns EKS add-on.
+ */
+export class CoreDnsAddOn extends CoreAddon {
+  constructor(scope: Construct, id: string, props: CoreDnsAddOnProps) {
+    super(scope, id, {
+      version: props.version || 'auto',
+      ...defaultProps,
+      ...props,
+    });
+  }
+}
--- stacks/Eks.ts
+++ stacks/Eks.ts
@@ -5,64 +5,154 @@ import {
   CapacityType,
   AuthenticationMode,
   EndpointAccess,
+  NodegroupAmiType,
 } from 'aws-cdk-lib/aws-eks';
-import {KubectlV30Layer} from '@aws-cdk/lambda-layer-kubectl-v30';
+import {InstanceType, SubnetType} from 'aws-cdk-lib/aws-ec2';
 
 import {use} from '../lib/app';
 import {Vpc} from './Vpc';
 import {StackContext} from '../lib/app';
-import {InstanceType, SubnetType} from 'aws-cdk-lib/aws-ec2';
 
 interface EksProps {
-  readonly kubernetesVersion: 'V1_29' | 'V1_30';
+  /**
+   * Kubernetes cluster version.
+   *
+   * @default - Defaults Kubernetes version 1.30
+   */
+  readonly kubernetesVersion?: 'V1_29' | 'V1_30';
+  /**
+   *  The Name of the created EKS Cluster.
+   */
   readonly clusterName: string;
-  readonly endpointAccess?: string[];
-  readonly nodeGroups: {
+  /**
+   * The allowed IPs access the EKS cluster.
+   */
+  readonly allowedListIps?: string[];
+  /**
+   * EKS managed Node groups.
+   */
+  readonly nodeGroups?: {
+    /**
+     * The Name of Node group.
+     */
     readonly name: string;
-    readonly os: 'AL2' | 'BOTTLEROCKET';
-    readonly capacityType: CapacityType;
+    /**
+     * The AMI type for your node group.
+     *
+     * @default - AL2_X86_64
+     */
+    readonly iamType?: `${NodegroupAmiType}`;
+    /**
+     * Capacity type of the managed node group
+     *
+     * @default - on-demand instances
+     */
+    readonly capacityType: `${CapacityType}`;
+    /**
+     * Instance type of the instances to start
+     *
+     * @default - m5.xlarge
+     */
     readonly instanceTypes: string[];
-    readonly desiredSize: number;
-    readonly maxSize: number;
-    readonly maxUnavailable?: number;
+    /**
+     * The current number of worker nodes that the managed node group should maintain. If not specified,
+     * the nodewgroup will initially create `minSize` instances.
+     *
+     * @default 1
+     */
+    readonly desiredSize?: number;
+    /**
+     * The maximum number of worker nodes that the managed node group can scale out to.
+     *
+     * @default - desiredSize
+     */
+    readonly maxSize?: number;
+    /**
+     * The maximum percentage of nodes unavailable during a version update.
+     * This percentage of nodes will be updated in parallel, up to 100 nodes at once.
+     *
+     * @default undefined - node groups will update instances one at a time
+     */
+    readonly maxUnavailablePercentage?: number;
+    /**
+     * The Kubernetes labels to be applied to the nodes in the node group when they are created.
+     *
+     * @default - None
+     */
     readonly labels?: {
       [name: string]: string;
     };
+    /**
+     * The Kubernetes taints to be applied to the nodes in the node group when they are created.
+     *
+     * @default - None
+     */
     readonly taints?: TaintSpec[];
   }[];
+  /**
+   * EKS add-ons
+   */
+  readonly addons?: {
+    readonly name: string;
+    readonly version?: string;
+  }[];
 }
+
+const kubernetesVersions: {[key: string]: KubernetesVersion} = {
+  V1_29: KubernetesVersion.V1_29,
+  V1_30: KubernetesVersion.V1_30,
+};
+
+const defaultProps = {
+  kubernetesVersion: KubernetesVersion.V1_30,
+  nodeGroupIamType: NodegroupAmiType.AL2_X86_64,
+  capacityType: 'ON_DEMAND',
+  instanceTypes: ['m5.xlarge'],
+  desiredSize: 1,
+} as const;
 
 export function EKS({stack, props}: StackContext<EksProps>) {
   const {vpc} = use(Vpc);
 
+  const version = props.kubernetesVersion
+    ? kubernetesVersions[props.kubernetesVersion]
+    : defaultProps.kubernetesVersion;
+
+  const endpointAccess = props.allowedListIps
+    ? EndpointAccess.PUBLIC_AND_PRIVATE.onlyFrom(...props.allowedListIps)
+    : undefined;
+
   const cluster = new EKSCluster(stack, props.clusterName, {
     clusterName: props.clusterName,
-    version:
-      props.kubernetesVersion === 'V1_29'
-        ? KubernetesVersion.V1_29
-        : KubernetesVersion.V1_30,
+    version,
+    vpc,
     defaultCapacity: 0,
-    vpc: vpc,
+    endpointAccess,
     vpcSubnets: [{subnetType: SubnetType.PRIVATE_WITH_EGRESS}],
-    kubectlLayer: new KubectlV30Layer(stack, 'kubectl'),
-    authenticationMode: AuthenticationMode.API,
-    endpointAccess: props.endpointAccess
-      ? EndpointAccess.PUBLIC_AND_PRIVATE.onlyFrom(...props.endpointAccess)
-      : undefined,
+    authenticationMode: AuthenticationMode.API_AND_CONFIG_MAP,
   });
 
-  for (const nodeGroupProps of props.nodeGroups) {
-    cluster.addNodegroupCapacity(nodeGroupProps.name, {
-      nodegroupName: nodeGroupProps.name,
-      capacityType: nodeGroupProps.capacityType,
-      desiredSize: nodeGroupProps.desiredSize,
-      maxSize: nodeGroupProps.maxSize,
-      maxUnavailablePercentage: nodeGroupProps.maxUnavailable,
-      instanceTypes: nodeGroupProps.instanceTypes.map(
+  if (props.nodeGroups) {
+    for (const nodeGroupProps of props.nodeGroups) {
+      const amiType = (nodeGroupProps.iamType || defaultProps.nodeGroupIamType) as NodegroupAmiType;
+      const capacityType = (nodeGroupProps.capacityType ||
+        defaultProps.capacityType) as CapacityType;
+      const desiredSize = nodeGroupProps.desiredSize || defaultProps.desiredSize;
+      const instanceTypes = (nodeGroupProps.instanceTypes || defaultProps.instanceTypes).map(
         instanceType => new InstanceType(instanceType)
-      ),
-      labels: nodeGroupProps.labels,
-      taints: nodeGroupProps.taints,
-    });
+      );
+
+      cluster.addNodegroupCapacity(nodeGroupProps.name, {
+        nodegroupName: nodeGroupProps.name,
+        amiType,
+        capacityType,
+        desiredSize,
+        instanceTypes,
+        maxSize: nodeGroupProps.maxSize,
+        labels: nodeGroupProps.labels,
+        taints: nodeGroupProps.taints,
+        maxUnavailablePercentage: nodeGroupProps.maxUnavailablePercentage,
+      });
+    }
   }
 }
